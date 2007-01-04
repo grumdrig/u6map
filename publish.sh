de611544566df157c375fd10d4e36c07af616c0a
Eric Fredricksen <eric@fredricksen.net>
2007-01-04 03:27:21 +0000

    forgot jpg
    git-svn-id: svn+ssh://grumdrig.com/svn/trunk/src/u6map@340 b1efd146-170f-0410-8dc8-cea33ec6b4df

--- publish.sh
+++ publish.sh
@@ -1,2 +1,2 @@
 #! /usr/bin/sh
-rsync -vaC --delete --delete-excluded --include="*.png" --include="*.xul" --include="*.css" --include="*.js" --include="*.php" --exclude="*" ./ grumdrig.com:/www/grumdrig.com/www/u6map/
+rsync -vaC --delete --delete-excluded --include="*.png" --include="*.xul" --include="*.css" --include="*.js" --include="*.php" --include="*.jpg" --exclude="*" ./ grumdrig.com:/www/grumdrig.com/www/u6map/
